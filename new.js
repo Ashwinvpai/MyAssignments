8466d4c5b4ca1ea39fcae14d78ceebfdce04dbce
Ashwin <ashpai411@gmail.com>
2020-07-05 12:05:02 +0000

    Initial Commit

---
+++ new.js
@@ -0,0 +1,18 @@
+var i = 1;
+alert("Value of i"+i);
+var i =[30,true,"Ashwin",12.5];
+alert("Value of Array",i);
+alert("Value of element at 1st Position",i[0]);
+alert("Value of element at 1st Position",i[1]);
+alert("Value of element at 1st Position",i[2]);
+alert("Value of element at 1st Position",i[3]);
+var emp = {
+    id:10,
+    name:"Ashwin"
+   Salary:5000
+    
+};
+Console.alert("VAlue of employee",emp);
+Console.alert("Employee Id",emp.id);
+Console.alert("Employee Name",emp.name);
+Console.alert("Employee Salary",emp.Salary);
