045e58b6423771c4fc216b02f1eee81bfe19da79
Ashwin <ashpai411@gmail.com>
2020-07-05 12:17:51 +0000

    Changed Js File again

--- new.js
+++ new.js
@@ -1,11 +1,12 @@
+alert("This is my new web page");
 var i = 1;
 alert("Value of i"+i);
 var i =[30,true,"Ashwin",12.5];
-alert("Value of Array",i);
-alert("Value of element at 1st Position",i[0]);
-alert("Value of element at 1st Position",i[1]);
-alert("Value of element at 1st Position",i[2]);
-alert("Value of element at 1st Position",i[3]);
+Console.alert("Value of Array",i);
+Console.alert("Value of element at 1st Position",i[0]);
+Console.alert("Value of element at 1st Position",i[1]);
+Console.alert("Value of element at 1st Position",i[2]);
+Console.alert("Value of element at 1st Position",i[3]);
 var emp = {
     id:10,
     name:"Ashwin",
