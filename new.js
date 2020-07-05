cc03fb5a5ecfbe8f4c7ab5bd015ff68c690b4d06
Ashwin <ashpai411@gmail.com>
2020-07-05 12:14:24 +0000

    Changed js file

--- new.js
+++ new.js
@@ -8,11 +8,15 @@ alert("Value of element at 1st Position",i[2]);
 alert("Value of element at 1st Position",i[3]);
 var emp = {
     id:10,
-    name:"Ashwin"
-   Salary:5000
+    name:"Ashwin",
+   Salary:5000,
+   dept:120
     
 };
 Console.alert("VAlue of employee",emp);
 Console.alert("Employee Id",emp.id);
 Console.alert("Employee Name",emp.name);
 Console.alert("Employee Salary",emp.Salary);
+Console.alert("Employee Department",emp.dept);
+delete emp.dept;
+Console.alert("Employee Details",emp);
