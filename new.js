7d457f8e41b20ed6a8c2fb6329d9216b9929cf60
Ashwin <ashpai411@gmail.com>
2020-07-05 13:52:50 +0000

    Changed js and index file

--- new.js
+++ new.js
@@ -7,28 +7,32 @@ console.log("Value of element at 1st Position",i[0]);
 console.log("Value of element at 1st Position",i[1]);
 console.log("Value of element at 1st Position",i[2]);
 console.log("Value of element at 1st Position",i[3]);
-var emp = {
+var employee = {
     id:10,
     name:"Ashwin",
    Salary:5000,
    dept:120
     
 };
-console.log("VAlue of employee",emp);
-console.log("Employee Id",emp.id);
-console.log("Employee Name",emp.name);
-console.log("Employee Salary",emp.Salary);
-console.log("Employee Department",emp.dept);
+console.log("VAlue of employee",employee);
+console.log("Employee Id",employee.id);
+console.log("Employee Name",employee.name);
+console.log("Employee Salary",employee.Salary);
+console.log("Employee Department",employee.dept);
 
 
-console.log("Employee Details",emp);
-var emp1 =emp;
+console.log("Employee Details",employee);
+var emp1 =employee;
 delete emp1.dept;
 console.log("Employee 1 details",emp1);
 const  pi =3.14;
 console.log("Value of pi",pi);
-const obj ={
-    1:"john",
-    2:"Steve"
+const emp ={
+    id:101,
+    name:"Steve",
+    Salary: 10000
+
 };
-console.log("Value of object",obj);
+console.log("employee details ",emp);
+emp.id=999;
+console.log("Changed Employee Details ",emp);
